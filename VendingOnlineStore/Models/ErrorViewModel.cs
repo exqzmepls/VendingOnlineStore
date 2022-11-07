800b8719b3a06150b17ffcb5d8bf7d900dc15433
exqzmepls <hm>
2022-11-07 19:45:01 +0500

    Добавьте файлы проекта.

---
+++ VendingOnlineStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendingOnlineStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
