76924f2588e1bb727e5f764f2cfbe9cd6388ca0f
exqzmepls <anton200159@yandex.ru>
2022-11-17 19:06:19 +0500

    Add test payment (#2)

---
+++ VendingOnlineStore/Controllers/BagController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendingOnlineStore.Clients.Payment;
+
+namespace VendingOnlineStore.Controllers;
+
+public class BagController : Controller
+{
+    private readonly IPaymentClient _paymentClient;
+
+    public BagController(IPaymentClient paymentClient)
+    {
+        _paymentClient = paymentClient;
+    }
+
+    public async Task<IActionResult> Buy(string id)
+    {
+        var url = await _paymentClient.CreatePayment();
+        return Redirect(url);
+    }
+}
