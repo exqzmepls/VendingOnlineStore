193c90e449b38f7463419bea26fa94b9e984bd1f
exqzmepls <exqzmepls@yandex.ru>
2023-03-14 19:42:27 +0500

    Change DB provider

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -7,7 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, string? connectionString)
     {
-        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
         return services;
     }
 }
